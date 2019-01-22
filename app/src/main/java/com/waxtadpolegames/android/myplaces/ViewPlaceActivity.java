04e5cea10446130f4b7316b1a3000def2d206b1b
azarrias <adolfo.zarrias@gmail.com>
2019-01-22 21:54:18 +0100

    Hide views if there is no data to be displayed on them

--- app/src/main/java/com/waxtadpolegames/android/myplaces/ViewPlaceActivity.java
+++ app/src/main/java/com/waxtadpolegames/android/myplaces/ViewPlaceActivity.java
@@ -2,6 +2,7 @@ package com.waxtadpolegames.android.myplaces;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -28,14 +29,40 @@ public class ViewPlaceActivity extends AppCompatActivity {
         typeLogo.setImageResource(place.getType().getResource());
         TextView type = findViewById(R.id.type);
         type.setText(place.getType().getText());
-        TextView address = findViewById(R.id.address);
-        address.setText(place.getAddress());
-        TextView phone = findViewById(R.id.phone);
-        phone.setText(place.getPhone());
-        TextView url = findViewById(R.id.url);
-        url.setText(place.getUrl());
-        TextView comment = findViewById(R.id.comment);
-        comment.setText(place.getComment());
+
+        if (place.getAddress().isEmpty()) {
+            findViewById(R.id.address_logo).setVisibility(View.GONE);
+            findViewById(R.id.address).setVisibility(View.GONE);
+        } else {
+            //findViewById(R.id.address).setVisibility(View.VISIBLE);
+            TextView address = findViewById(R.id.address);
+            address.setText(place.getAddress());
+        }
+
+        if (place.getPhone().isEmpty()) {
+            findViewById(R.id.phone_logo).setVisibility(View.GONE);
+            findViewById(R.id.phone).setVisibility(View.GONE);
+        } else {
+            TextView phone = findViewById(R.id.phone);
+            phone.setText(place.getPhone());
+        }
+
+        if (place.getUrl().isEmpty()) {
+            findViewById(R.id.url_logo).setVisibility(View.GONE);
+            findViewById(R.id.url).setVisibility(View.GONE);
+        } else {
+            TextView url = findViewById(R.id.url);
+            url.setText(place.getUrl());
+        }
+
+        if (place.getComment().isEmpty()) {
+            findViewById(R.id.comment_logo).setVisibility(View.GONE);
+            findViewById(R.id.comment).setVisibility(View.GONE);
+        } else {
+            TextView comment = findViewById(R.id.comment);
+            comment.setText(place.getComment());
+        }
+
         TextView date = findViewById(R.id.date);
         date.setText(DateFormat.getDateInstance().format(new Date(place.getDate())));
         TextView time = findViewById(R.id.time);
